66aeb25f9f560828f05f8769bbc818cfefe5d359
Hassan Alzourei <hassanalzourei@gmail.com>
2026-07-15 19:03:57 +0300

    update navLinks

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,11 @@ export const navLinks = [
         name: 'Contact',
         href: '#contact',
     },
+    {
+        id: 5,
+        name: 'Experience',
+        href: '#experience',
+    },
 ];
 
 export const clientReviews = [
