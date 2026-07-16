4fe30e33dbc695d2ef3779bd984ce0b6554327e5
Hassan Alzourei <hassanalzourei@gmail.com>
2026-07-17 00:24:12 +0300

    update About add profile image

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src={`${import.meta.env.BASE_URL}assets/grid1.png`} alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+                        <img src={`${import.meta.env.BASE_URL}assets/profile-pic.jpeg`} alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
                         <div>
                             <p className="grid-headtext">Hi, I am Hassan Mohammed Alzourei</p>
                             <p className="grid-subtext">
