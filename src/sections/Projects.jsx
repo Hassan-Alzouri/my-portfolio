6d52335e377980bda91575f859b4afddb6534655
Hassan Alzourei <hassanalzourei@gmail.com>
2026-07-26 00:12:24 +0300

    make the spotlight purely decorative for pointer events

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -49,7 +49,7 @@ const Projects = () => {
             <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
                 <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
                     {currentProject.spotlight && (
-                        <div className="absolute top-0 right-0">
+                        <div className="absolute top-0 right-0 -z-10 pointer-events-none">
                             {/* Updated with getAssetPath */}
                             <img src={getAssetPath(currentProject.spotlight)} alt="spotlight" className="w-full h-96 object-cover rounded-xl" />
                         </div>
